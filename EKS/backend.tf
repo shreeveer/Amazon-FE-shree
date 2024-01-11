1b91f168a651317379b570841d5d8c828a647e46
shreeveer <147177545+shreeveer@users.noreply.github.com>
2024-01-12 01:36:16 +0530

    Update backend.tf

--- EKS/backend.tf
+++ EKS/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "infra-terraform-bucket-v1"  # Replace with your actual S3 bucket name
+    bucket = "infra-terraform-s3-123"  # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"  
-    region = "ap-south-1" 
+    region = "us-west-1" 
   }
 }
